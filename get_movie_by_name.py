3173f977e0a94675b94865cf593b8bb85f94ebd7
vystrcild <vystrcil.dusan@gmail.com>
2021-01-27 14:41:27 +0100

    Cleaning code

--- get_movie_by_name.py
+++ get_movie_by_name.py
@@ -11,13 +11,16 @@ load_dotenv()
 TMDB_API_KEY = os.environ["TMDB_API_KEY"]
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                             'Chrome/79.0.3945.117 Safari/537.36'}
+                         'Chrome/79.0.3945.117 Safari/537.36'}
+
 
 def get_movie_from_tmdb_by_name(title, year):
-    url_api = f"https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&language=en-US&query={title}&page=1&include_adult=false&year={year}"
+    url_api = f"https://api.themoviedb.org/3/search/movie?api_key={TMDB_API_KEY}&language=en-US&query={title}" \
+              f"&page=1&include_adult=false&year={year}"
     r = requests.get(url_api).json()
     movie_id = r["results"][0]["id"]
     return movie_id
+
 
 def get_movie_from_csfd(url):
     r_movie = requests.get(url, headers=headers)
@@ -26,7 +29,8 @@ def get_movie_from_csfd(url):
     year = soup.find("p", class_="origin").find("span").get_text()
     return title, year
 
-# Script input
+
+# Script input - movie url (CSFD)
 url = sys.argv[1]
 
 # Get movie details from CSFD
@@ -48,25 +52,25 @@ json_ouput = {"items": [
             "path": "youtube.png"
         }
     },
-        {
-            "uid": "watchlist",
-            "title": "Add To Watchlist",
-            "subtitle": f"{query_movie.title} {query_movie.release_year}",
-            "arg": "MOVIE ID",
-            "icon": {
-                "path": "watchlist.png"
-            }
-        },
     {
-            "uid": "torrent",
-            "title": "Get Torrent",
-            "subtitle": f"{query_movie.title} {query_movie.release_year}",
-            "arg": "MOVIE ID",
-            "icon": {
-                "path": "torrent.png"
-            }
+        "uid": "watchlist",
+        "title": "Add To Watchlist",
+        "subtitle": f"{query_movie.title} {query_movie.release_year}",
+        "arg": "MOVIE ID",
+        "icon": {
+            "path": "watchlist.png"
         }
-    ]}
+    },
+    {
+        "uid": "torrent",
+        "title": "Get Torrent",
+        "subtitle": f"{query_movie.title} {query_movie.release_year}",
+        "arg": "MOVIE ID",
+        "icon": {
+            "path": "torrent.png"
+        }
+    }
+]}
 
 # Print output for Alfred Workflow
 print(json.dumps(json_ouput))
