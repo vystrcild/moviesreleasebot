a255702a0e1fed9385ac967db5fea4902e8f6576
vystrcild <vystrcil.dusan@gmail.com>
2021-01-27 15:49:09 +0100

    Get torrent fcions proper format of output

--- get_torrent.py
+++ get_torrent.py
@@ -6,16 +6,26 @@ def get_torrent_yts(query):
     # List all query results from YTS.mx
     url = f"https://yts.mx/api/v2/list_movies.json?query_term={query}"
     r = requests.get(url).json()
+    results = []
     for movie in r["data"]["movies"]:
         title = movie["title"]
         year = movie["year"]
-        print(title, year)
+        for torrent in movie["torrents"]:
+            if torrent["quality"] == "1080p":
+                torrent_url = torrent["url"]
+                size = torrent["size"]
+        torrent_name = f"{title} - {year} - {size}"
+        results.append({"name":torrent_name, "url":torrent_url})
+    return results
+
 
 def get_torrent_rarbg(query):
     # List all query results from RARBG.to
     client = rarbgapi.RarbgAPI()
     search = client.search(search_string=query, categories=[rarbgapi.RarbgAPI.CATEGORY_MOVIE_H264_1080P])
-    for movie in search:
-        print(movie.download, movie.filename)
+    results = []
+    for torrent in search:
+        results.append({"name": torrent.filename, "url": torrent.download})
+    return results
 
-get_torrent_rarbg("Terminator 2 1991")
+print(get_torrent_rarbg("Soul 2020"))
